bcf6d0fc6eb06e064a1f5384f5c861e806ce9551
KalahariKanga <djfhill@mailinator.com>
2014-12-12 14:15:56 +0000

    Cleaned up main

--- ooImages.cpp
+++ ooImages.cpp
@@ -16,32 +16,25 @@ int main(int argc, _TCHAR* argv[])
 	sf::Texture texture;
 	sf::Sprite sprite;
 
-	/*ImageObject image;
-	image.loadImage("C:\\Users\\David\\Pictures\\test.bmp");
-	Selection selection(image.getWidth(),image.getHeight());
-	selection.toRegion(&image, "x<y");
-	Invert invert(&image, &selection);
-	invert.modify();*/
-
-
 	ImageStore store;
 	store.loadImage("C:\\Users\\David\\Pictures\\test.bmp");
 	Selection selection(store.image->getWidth(), store.image->getHeight());
 	Interpreter interpreter;
 	interpreter.selection = &selection; 
 	interpreter.store = &store;
+
 	std::string input;
 	while (1)
 	{
-		std::cout << ">>";
-		std::getline(std::cin,input);
-		interpreter.interpret(input);
 		texture.loadFromImage(*(store.image->getImage()));
 		sprite.setTexture(texture);
 		window.draw(sprite);
 		window.display();
+		std::cout << ">>";
+		std::getline(std::cin,input);
+		interpreter.interpret(input);
 	}
-	//system("pause");
+
 	return 0;
 }
 
