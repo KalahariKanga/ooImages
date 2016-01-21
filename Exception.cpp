6e60164b9769309554569a9a36f83516fb06d2ad
FleaRex <bmad29@hotmail.co.uk>
2016-01-21 13:54:14 +0000

    Gives motivational comment in exception.
    You can do better than that.

--- Exception.cpp
+++ Exception.cpp
@@ -15,28 +15,28 @@ std::string Exception::getErrorString()
 	switch (errortype)
 	{
 	case ErrorType::TYPE_MISMATCH:
-		return std::string("Parser error: type mismatch\n");
+		return std::string("You're being shit. Parser error: type mismatch\n");
 	case ErrorType::INSUFFICIENT_ARGUMENTS:
-		return std::string("Parser error: insufficient arguments\n");
+		return std::string("You're being shit. Parser error: insufficient arguments\n");
 	case ErrorType::ILLEGAL_OPERATOR:
-		return std::string("Parser error: illegal operator\n");
+		return std::string("You're being shit. Parser error: illegal operator\n");
 	case ErrorType::ARRAY_ERROR:
-		return std::string("Parser error: error accessing array\n");
+		return std::string("You're being shit. Parser error: error accessing array\n");
 	case ErrorType::ILLEGAL_NAME:
-		return std::string("Parser error: illegal variable name\n");
+		return std::string("You're being shit. Parser error: illegal variable name\n");
 	case ErrorType::MISMATCHED_BRACKETS:
-		return std::string("Parser error: mismatched brackets\n");
+		return std::string("You're being shit. Parser error: mismatched brackets\n");
 	case ErrorType::UNKNOWN_PROPERTY:
-		return std::string("Parser error: unknown property\n");
+		return std::string("You're being shit. Parser error: unknown property\n");
 	case ErrorType::UNKNOWN_VARIABLE:
-		return std::string("Parser error: unknown variable\n");
+		return std::string("You're being shit. Parser error: unknown variable\n");
 	case ErrorType::FILE_SAVE_ERROR:
-		return std::string("File Save error\n");
+		return std::string("You're being shit. File Save error\n");
 	case ErrorType::FILE_LOAD_ERROR:
-		return std::string("File Load error\n");
+		return std::string("You're being shit. File Load error\n");
 	case ErrorType::LOOP_ERROR:
-		return std::string("Loop error\n");
+		return std::string("You're being shit. Loop error\n");
 	default:
-		return std::string("Parser error: unknown\n");
+		return std::string("You're being shit. Parser error: unknown\n");
 	}
 }
