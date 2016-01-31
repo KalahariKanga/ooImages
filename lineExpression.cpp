0e4d35ba006167a234c4975d3be24f00855d8cf4
KalahariKanga <djfhill@mailinator.com>
2016-01-31 13:24:04 +0000

    better line selections
    still a bit wonky

--- lineExpression.cpp
+++ lineExpression.cpp
@@ -21,28 +21,101 @@ Variable lineExpression::evaluate()
 	float x2 = *arguments[2]->getResult().get<float>();
 	float y2 = *arguments[3]->getResult().get<float>();
 
+	Variable var(Variable::Type::Mask);
+	Mask* s = new Mask(image->getWidth(), image->getHeight());
 	float minX = std::min(x1, x2);
 	float maxX = std::max(x1, x2);
 	float minY = std::min(y1, y2);
 	float maxY = std::max(y1, y2);
 
-	Variable var(Variable::Type::Mask);
-	Mask* s = new Mask(image->getWidth(), image->getHeight());
-	
-	float d = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int dx1 = std::abs(dx);
+	int dy1 = std::abs(dy);
+	int px = 2 * dy1 - dx1;
+	int py = 2 * dx1 - dy1;
+	int x, y, xe, ye;
+
 	for (int cx = 0; cx < image->getWidth(); cx++)
 		for (int cy = 0; cy < image->getHeight(); cy++)
-		{
 			s->setValue(cx, cy, 0);
-			if (cx >= minX && cx <= maxX && cy >= minY && cy <= maxY)
-			{
-				float d1 = sqrt((cx - x1)*(cx - x1) + (cy - y1)*(cy - y1));
-				float d2 = sqrt((cx - x2)*(cx - x2) + (cy - y2)*(cy - y2));
-				if (abs(d1 + d2 - d) < 0.0001)//epsilon
-					s->setValue(cx, cy, 1);
-			}
+
+	if (dy1 <= dx1)
+	{
+		if (dx >= 0)
+		{
+			x = x1;
+			y = y1;
+			xe = x2;
 		}
+		else
+		{
+			x = x2;
+			y = y2;
+			xe = x1;
+		}
+		s->setValue(x, y, 1);
+		for (int i = 0; x < xe; i++)
+		{
+			x = x + 1;
+			if (px < 0)
+			{
+				px = px + 2 * dy1;
+			}
+			else
+			{
+				if ((dx < 0 && dy < 0) || (dx>0 && dy>0))
+				{
+					y = y + 1;
+				}
+				else
+				{
+					y = y - 1;
+				}
+				px = px + 2 * (dy1 - dx1);
+			}
+			s->setValue(x, y, 1);
+		}
+	}
+	else
+	{
+		if (dy >= 0)
+		{
+			x = x1;
+			y = y1;
+			ye = y2;
+		}
+		else
+		{
+			x = x2;
+			y = y2;
+			ye = y1;
+		}
+		s->setValue(x, y, 1);
+		for (int i = 0; y < ye; i++)
+		{
+			y = y + 1;
+			if (py <= 0)
+			{
+				py = py + 2 * dx1;
+			}
+			else
+			{
+				if ((dx < 0 && dy < 0) || (dx>0 && dy>0))
+				{
+					x = x + 1;
+				}
+				else
+				{
+					x = x - 1;
+				}
+				py = py + 2 * (dx1 - dy1);
+			}
+			s->setValue(x, y, 1);
+		}
+	}
 	var.set<Mask>(s);
 	return var;
 
 }
+
