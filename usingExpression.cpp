9f73069ad6b17499062b0bef85ea81c2adf18407
KalahariKanga <djfhill@mailinator.com>
2016-03-21 14:49:54 +0000

    usingExpression was hilariously overcomplicated
    works for function calls and arrays and stuff now

--- usingExpression.cpp
+++ usingExpression.cpp
@@ -1,6 +1,5 @@
 #include "usingExpression.h"
 #include "ImageStore.h"
-#include "TerminalExpression.h"
 
 usingExpression::usingExpression()
 {
@@ -14,16 +13,6 @@ usingExpression::~usingExpression()
 
 Variable usingExpression::evaluate()
 {
-	auto term = dynamic_pointer_cast<TerminalExpression>(arguments[0]);
-	//be careful if its a local variable
-	//or function call
-	if (term)
-	{
-		Variable image = VariableStore::get()->getRawVariable(term->getName());
-		store->image = image.getShared<ImageObject>();
-	}
-	else
-		store->image = arguments[0]->getResult().getShared<ImageObject>();
+	store->image = arguments[0]->getResult().getShared<ImageObject>();
 	return Variable();
-
 }
