232e0ce88761c51cfbe68ac70c06bbd062e6f9f4
KalahariKanga <djfhill@mailinator.com>
2016-03-26 11:07:53 +0000

    array workaround
    all arrays are global for now

--- setExpression.cpp
+++ setExpression.cpp
@@ -1,4 +1,5 @@
 #include "setExpression.h"
+#include "ArrayVariableExpression.h"
 
 setExpression::setExpression()
 {
@@ -18,8 +19,8 @@ Variable setExpression::evaluate()
 	//for normal variables, rawname == basename
 	//for arrays, rawname = index##basename
 
-	//TODO: check for illegal characters
 	auto term = std::dynamic_pointer_cast<TerminalExpression>(arguments[0]);
+	auto arr = std::dynamic_pointer_cast<ArrayVariableExpression>(arguments[0]);
 	if (term)
 	{
 		rawname = term->getName();
@@ -27,11 +28,11 @@ Variable setExpression::evaluate()
 	}
 	else
 		throw new Exception(Exception::ErrorType::ILLEGAL_NAME);
-
+	
 	if (VariableStore::get()->legalVariableName(basename))
 	{
 		var = arguments[1]->getResult().duplicate();
-		if (parent)
+		if (parent && !arr)
 			parent->setLocalVariable(rawname, var);
 		else
 			VariableStore::get()->setVariable(rawname, var);
