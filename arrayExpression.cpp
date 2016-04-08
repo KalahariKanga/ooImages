b09e34b081f7ac52dacf6cb0a471dfbe1ef1d317
KalahariKanga <djfhill@mailinator.com>
2016-04-08 16:57:39 +0100

    array constructor
    similar to kernel constructor

--- arrayExpression.cpp
+++ arrayExpression.cpp
@@ -1,9 +1,9 @@
 #include "arrayExpression.h"
-
+#include "SequenceExpression.h"
 
 arrayExpression::arrayExpression()
 {
-	noArguments = 0;
+	noArguments = 1;
 }
 
 
@@ -13,5 +13,18 @@ arrayExpression::~arrayExpression()
 
 Variable arrayExpression::evaluate()
 {
-	return Variable(new Array());
+	auto sequence = std::dynamic_pointer_cast<SequenceExpression>(arguments[0]);
+
+	if (!sequence)
+		throw new Exception(Exception::ErrorType::MISMATCHED_BRACKETS);
+
+	int elements = sequence->noArguments;
+	Variable var(new Array());
+	auto a = var.get<Array>();
+
+	for (int c = 0; c < elements; c++)
+	{
+		a->set(c, sequence->arguments[c]->getResult());
+	}
+	return var;
 }
