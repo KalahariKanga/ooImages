9e768180100c777113ffad3c3c68a3a7a2d99046
KalahariKanga <djfhill@mailinator.com>
2016-05-01 23:19:11 +0100

    print real arrays

--- printExpression.cpp
+++ printExpression.cpp
@@ -1,5 +1,5 @@
 #include "printExpression.h"
-
+#include "Array.h"
 
 printExpression::printExpression()
 {
@@ -14,7 +14,21 @@ printExpression::~printExpression()
 
 Variable printExpression::evaluate()
 {
-	float value = *arguments[0]->getResult().get<Real>();
-	std::cout << value << std::endl;
+	auto result = arguments[0]->getResult();
+	if (result.isType<Real>())
+	{
+		float value = *result.get<Real>();
+		std::cout << value << std::endl;
+	}
+	
+	if (result.isType<Array>())
+	{
+		auto arr = result.get<Array>();
+		std::cout << "[ ";
+		for (int c = 0; c < arr->size(); c++)
+			std::cout << *arr->get(c).get<Real>() << " ";
+		std::cout << "]\n";
+	}
+
 	return Variable();
 }
